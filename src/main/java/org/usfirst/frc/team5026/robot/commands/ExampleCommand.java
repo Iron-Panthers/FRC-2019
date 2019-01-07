51fdefa0b89a6fd5025a114d198493cc34904eb9
Ingi Helgason <ingib0803@gmail.com>
2019-01-07 13:10:25 -0800

    Fix directory names

--- src/main/java/org/usfirst/frc/team5026/robot/commands/ExampleCommand.java
+++ src/main/java/org/usfirst/frc/team5026/robot/commands/ExampleCommand.java
@@ -5,10 +5,10 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot.commands;
+package org.usfirst.frc.team5026.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
-import frc.robot.Robot;
+import org.usfirst.frc.team5026.robot.Robot;
 
 /**
  * An example command.  You can replace me with your own command.
