1d8bcedc9d84c46ebcf523152cd16e9a12766387
Matthew Tran <mattgtran2001@gmail.com>
2019-02-10 15:11:20 -0800

    Sets Intake Arm Power to 0 when trigger is released

--- src/main/java/org/usfirst/frc/team5026/robot/subsystems/intake/commands/ManualArmMovement.java
+++ src/main/java/org/usfirst/frc/team5026/robot/subsystems/intake/commands/ManualArmMovement.java
@@ -55,13 +55,13 @@ public class ManualArmMovement extends Command {
 	// Called once after isFinished returns true
 	@Override
 	protected void end() {
-		//Robot.intakeArm.moveArm(basePower);
+		Robot.intakeArm.moveArm(0);
 	}
 
 	// Called when another command which requires one or more of the same
 	// subsystems is scheduled to run
 	@Override
 	protected void interrupted() {
-		// Robot.intakeArm.moveArm(basePower);
+		Robot.intakeArm.moveArm(0);
 	}
 }
