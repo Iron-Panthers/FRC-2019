19e64ed814eaa7dbfc1b3de6f293e558a5eb26a7
Ingi Helgason <ingib0803@gmail.com>
2019-02-26 17:01:30 -0800

    Add Batchfiles for driver station static IP/DHCP switching
    These require elevated cmd. Details about the expected response(s) can be found in greater detail in the files themselves.

---
+++ driverstation/dsdhcp.bat
@@ -0,0 +1,10 @@
+@echo off
+
+net session
+:: Exists to check for admin permissions
+:: If not enough permissions, puts "System error 5 has occured. ... Access is denied."
+:: If permissions are sufficient, puts something else (often, "There are no entries in the list")
+
+netsh interface ipv4 set address name="Wi-Fi" source=dhcp > nul
+:: Changing IPv4 interface named "Wi-Fi" to DHCP
+:: Needed to use IP addresses assigned automatically by a DHCP server, such as by a router
