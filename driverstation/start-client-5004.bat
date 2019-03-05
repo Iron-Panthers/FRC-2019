99a485274ad3a96c1efd2c2943b981731a9f88cb
Ingi Helgason <ingib0803@gmail.com>
2019-03-04 17:07:06 -0800

    Add pause in event of script failure

--- driverstation/start-client-5004.bat
+++ driverstation/start-client-5004.bat
@@ -6,3 +6,6 @@
 gst-launch-1.0 udpsrc port=5004 ! "application/x-rtp" ! rtph264depay ! avdec_h264 ! videoflip method=clockwise ! videoflip method=clockwise ! autovideosink
 :: Notably, this flips the camera feed vertically (top becomes bottom)
 :: You may need to install the avdec_h264 decoder, as it might not come with the default installation of GStreamer
+
+pause
+:: If the script fails then wait for confirmation before closing
