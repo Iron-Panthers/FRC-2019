1dec858c3093da60a837e21e356b92df12658024
X1 Carbon Driver Station <5026craptop@gmail.com>
2019-03-07 15:08:22 -0800

    Change subnet mask

--- driverstation/ethstatic.bat
+++ driverstation/ethstatic.bat
@@ -5,8 +5,8 @@ net session
 :: If not enough permissions, puts "System error 5 has occured. ... Access is denied."
 :: If permissions are sufficient, puts something else (often, "There are no entries in the list")
 
-netsh interface ipv4 set address name="Ethernet 6" static 10.50.26.5 255.0.0.0 10.50.26.1 > nul
+netsh interface ipv4 set address name="Ethernet 6" static 10.50.26.5 255.255.255.0 10.50.26.1 > nul
 :: Changing IPv4 interface named "Ethernet 6" to static, with the following configs:
 :: IP Address: 10.50.26.5
-:: Subnet Mask: 255.0.0.0
+:: Subnet Mask: 255.255.255.0
 :: Default Gateway: 10.50.26.1
