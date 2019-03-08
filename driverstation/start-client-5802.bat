23cb7b2f130cb97f55c5a994930af50c5f41e5ba
Ingi Helgason <ingib0803@gmail.com>
2019-03-08 07:07:53 -0800

    Switch to allowed UDP ports, rename GStreamer scripts accordingly
    Desktop shortcuts on the driver stations will have to be re-created. These ports (5801 + 5802) were tested yesterday (3/7/2019)

--- driverstation/start-client-5802.bat
+++ driverstation/start-client-5802.bat
@@ -1,9 +1,9 @@
 @echo off
 
-:: This can be used to start listening to a GStreamer UDP feed on port 5004
+:: This can be used to start listening to a GStreamer UDP feed on port 5802
 :: For convenience, creating a shortcut on the desktop to this script is recommended
 
-gst-launch-1.0 udpsrc port=5004 ! "application/x-rtp" ! rtph264depay ! avdec_h264 ! videoflip method=clockwise ! videoflip method=clockwise ! autovideosink
+gst-launch-1.0 udpsrc port=5802 ! "application/x-rtp" ! rtph264depay ! avdec_h264 ! videoflip method=clockwise ! videoflip method=clockwise ! autovideosink
 :: Notably, this flips the camera feed vertically (top becomes bottom)
 :: You may need to install the avdec_h264 decoder, as it might not come with the default installation of GStreamer
 
