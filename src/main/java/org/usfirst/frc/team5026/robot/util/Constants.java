d13b16d3be57ca53c1074d83749bb8be94d1d364
X1 Carbon Driver Station <5026craptop@gmail.com>
2019-03-17 17:07:35 -0700

    Button 9 Good for picking up Hatches, Button 4 for Holding Them

--- src/main/java/org/usfirst/frc/team5026/robot/util/Constants.java
+++ src/main/java/org/usfirst/frc/team5026/robot/util/Constants.java
@@ -60,7 +60,7 @@ public class Constants {
         public static final int INTAKE_BUTTON = 2;
         public static final int OUTTAKE_BUTTON = 3;
         // Arm Setpoints
-        public static final int HATCH_LOADING_HEIGHT_BUTTON = 4;
+        public static final int HATCH_HOLDING_HEIGHT_BUTTON = 4;
         public static final int ZERO_INTAKE_ARM_BUTTON = 7;
         public static final int LOWEST_HEIGHT_BUTTON = 8;
 		public static final int OPP_ROCKET_LOW_HEIGHT_BUTTON = 9; // DO NOT USE
@@ -114,7 +114,7 @@ public class Constants {
         public static final double ARM_LENGTH = 27.4; // in
 		public static final double ARM_BASE_HEIGHT = 18.75; // in
         public static final double HATCH_HEIGHT = 18.75 - ARM_BASE_HEIGHT;
-        public static final double HATCH_LOADING_HEIGHT = 12.5 - ARM_BASE_HEIGHT;
+        public static final double HATCH_HOLDING_HEIGHT = 30.0 - ARM_BASE_HEIGHT;
         public static final double CARGO_SHIP_HEIGHT = 39.0 - ARM_BASE_HEIGHT + CARGO_DIAMETER; // in
         public static final double ROCKET_LOW_HEIGHT = 34.0 - ARM_BASE_HEIGHT; // in
         public static final double CARGO_SHIP_FRONT_BACK_ADJUST = 9; // in
