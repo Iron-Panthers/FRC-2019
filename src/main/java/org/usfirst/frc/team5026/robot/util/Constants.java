49e67e956e577bd04ae4104cc7b22472e62a6f2a
X1 Carbon Driver Station <5026craptop@gmail.com>
2019-05-18 10:14:12 -0700

    Baby Proofing Maker Faire

--- src/main/java/org/usfirst/frc/team5026/robot/util/Constants.java
+++ src/main/java/org/usfirst/frc/team5026/robot/util/Constants.java
@@ -25,7 +25,9 @@ public class Constants {
                                                                    // the front
         public static final double TURN_SENSITIVITY = 1;
 		public static final double RADIAL_TURN_SENSITIVITY = 20;
-		public static final double RAMP_RATE = 0.25; // Seconds to go from 0 to full throttle
+        public static final double RAMP_RATE = 0.25; // Seconds to go from 0 to full throttle
+        public static final boolean IS_BABY_PROOFED = true;
+        public static final double BABY_PROOF_MODIFIER = 0.25;
 
         // Max Velocity in RPM
         // Low gear (High RPM, low robot speed) // Tested 3/20/2019 by James
