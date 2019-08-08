f9006c3544ec27ffaa4f9d5a59fc63b6c605754b
X1 Carbon Driver Station <5026craptop@gmail.com>
2019-08-07 18:42:28 -0700

    Update Intake Arm P Value
    Change from 0.2 to 0.1, no more oscillation, but slower

--- src/main/java/org/usfirst/frc/team5026/robot/util/Constants.java
+++ src/main/java/org/usfirst/frc/team5026/robot/util/Constants.java
@@ -126,7 +126,7 @@ public class Constants {
 
         // INTAKE ARM PID
         public static final double INTAKE_ARM_MAX_POWER = 0.35;
-        public static final double INTAKE_ARM_P = 0.2;
+        public static final double INTAKE_ARM_P = 0.1; // 0.1 Works well, but slow on comp robot, 0.15-0.2 has too much oscillation, 0.125 okay, but some oscillation (James 8/7/2019)
         public static final double INTAKE_ARM_I = 0;
         public static final double INTAKE_ARM_D = 0;
         public static final double ERROR_TOLERANCE = 3; // degrees
