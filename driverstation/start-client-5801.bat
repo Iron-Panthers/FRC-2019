a6482f9cac9bb4085a88619573a6b525a42d9255
James Varah <james@varah.com>
2019-09-10 17:20:11 -0700

    Update Jetson Scripts for Nano, Untested

--- driverstation/start-client-5801.bat
+++ driverstation/start-client-5801.bat
@@ -6,7 +6,7 @@
 
 :: For convenience, creating a shortcut on the desktop to this script is recommended
 
-gst-launch-1.0 -e udpsrc port=5801 ! "application/x-rtp" ! rtph264depay ! avdec_h264 ! videoflip method=clockwise ! videoflip method=clockwise ! tee name=t t. ! queue ! autovideosink t. ! queue ! video/x-raw,width=640,height=480,framerate=30/1 ! x264enc speed-preset=1 tune=zerolatency bitrate=1024 ! mp4mux ! filesink location=%time:~0,2%_%time:~3,2%_%time:~6,2%__%date:~4,2%_%date:~7,2%_%date:~10,4%_5801.mp4
+gst-launch-1.0 -e udpsrc port=5801 ! "application/x-rtp, encoding-name=VP8" ! rtpvp8depay ! decodebin ! videoflip method=clockwise ! videoflip method=clockwise ! tee name=t t. ! queue ! autovideosink t. ! queue ! video/x-raw,width=640,height=480,framerate=30/1 ! x264enc speed-preset=1 tune=zerolatency bitrate=1024 ! mp4mux ! filesink location=%time:~0,2%_%time:~3,2%_%time:~6,2%__%date:~4,2%_%date:~7,2%_%date:~10,4%_5801.mp4
 :: Notably, this flips the camera feed vertically (top becomes bottom)
 :: You may need to install the avdec_h264 decoder, as it might not come with the default installation of GStreamer
 
